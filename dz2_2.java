e9b80ea92347b6cb63947ad4746f1685f21c2ee6
Vladimir <vlborshh@yandex.ru>
2023-07-18 15:09:08 +0300

    дз2_2

---
+++ dz2_2.java
@@ -0,0 +1,17 @@
+
+public class dz2_2 {
+    public static void main(String[] args) {
+        int[] array = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11 };
+        corrCode(array);
+    }
+
+    private static void corrCode(int[] intArray) {
+        try {
+            int d = 0;
+            double catchedRes1 = intArray[8] / d;
+            System.out.println("catchedRes1 = " + catchedRes1);
+        } catch (ArithmeticException e) {
+            System.out.println("Catching exception: " + e);
+        }
+    }
+}
