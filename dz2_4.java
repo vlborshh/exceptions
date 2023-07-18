254bedc08a324ba5d54fc67fb4dbf995d12c65d7
Vladimir <vlborshh@yandex.ru>
2023-07-18 15:30:40 +0300

    дз2_4

---
+++ dz2_4.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class dz2_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите строку: ");
+        String input = scanner.nextLine();
+
+        try {
+            if (input.isEmpty()) {
+                throw new Exception("Пустые строки вводить нельзя!");
+            } else {
+                System.out.println("Вы ввели: " + input);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
